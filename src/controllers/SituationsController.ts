389d2eea3741e08b1dee37c337182e4409ae38df
Gabryeldesa <gabryelds8@gmail.com>
2026-09-10 20:33:37 -0300

    atualização A5

--- src/controllers/SituationsController.ts
+++ src/controllers/SituationsController.ts
@@ -4,8 +4,50 @@ import { Situations } from '../entity/Situations';
 
 const router = express.Router();
 
-router.get('/situations', (req: Request, res: Response) => {
-  res.send('Bem vindo pessoal tela de situações da rota!');
+router.get('/situations', async(req: Request, res: Response) => {
+
+  try {
+  const situationRepository = AppDataSource.getRepository(Situations);
+
+  const situations = await situationRepository.find();
+
+  res.status(200).json(situations);
+  return;
+  
+  }catch (error) {
+    res.status(500).json({
+      message: 'Erro ao listar situações.',
+    });
+    return;
+  }
+});
+
+//Criar a Visualização do item cadastrado em situação
+router.get("/situations/:id", async (req:Request, res:Response)=>{
+  try{
+
+    const { id } = req.params as { id: string };
+
+    const situationRepository = AppDataSource.getRepository(Situations);
+
+    const situation = await situationRepository.findOneBy({id : parseInt(id)});
+
+    if(!situation){
+      res.status(404).json({
+        messagem : "Situação não encontrada!",
+      });
+      return
+    }
+
+    res.status(200).json(situation);
+    return
+
+  }catch(error){
+    res.status(500).json({
+      messagem : "Erro ao Listar situação!",
+    });
+    return
+  }
 });
 
 router.post('/situations', async(req: Request, res: Response) => {
